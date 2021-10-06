5abe8635ae2c5dd97b301387c59f83e1d90f8c5d
Unknown <tomlazelle@yahoo.com>
2021-10-06 16:06:26 -0500

    first checkin

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
+
+app.get("/newkids", (req, res, next) => {
+    res.json(["Donnie","Joey","Jordan","Jonathan","Danny","David"]);
+});
+
+app.get("/nsync", (req, res, next) => {
+    res.status(500);
+    res.send("Not Implemented");
+
+});
+
+app.get("/health", (req, res, next) => {
+    res.json("Active");
+});
